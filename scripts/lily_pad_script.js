fa2dbf646fed881d94eca30448130f651fcdd1b5
brenbil112 <128422728+brenbil112@users.noreply.github.com>
2025-03-01 21:57:17 -0500

    fixed script

--- scripts/lily_pad_script.js
+++ scripts/lily_pad_script.js
@@ -12,7 +12,7 @@ createBtn.addEventListener('click', () => {
     let img = document.createElement('img');
     inputBox.className = 'input-box';
     inputBox.setAttribute('contenteditable', 'true');
-    img.src = "images/delete.png";
+    img.src = "images/lily_pad_images/delete.png";
     notesContainer.appendChild(inputBox).appendChild(img);
     updateStorage();
 });
